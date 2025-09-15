d8d629519be5ac2f7ddcddc7e7ccf17a86369b79
Keval Tank <kevaltank1445@gmail.com>
2025-09-15 17:01:20 +0530

    Adjust ledger entry

--- middleware/requestId.js
+++ middleware/requestId.js
@@ -1,13 +1,18 @@
-import { idempotency } from './db.js'
+import { ledgerEntry, date} from './db.js'
 import {v4} from 'uuid';
+export let states = Object.freeze(['Pending', 'Cancelled', 'Done']);
 
 export async function addRequestId(req, res, next) {
     if (req.originalUrl === '/transfer') {
         req.requestId = v4();
-        await idempotency.insertOne({
-            transactionId: req.requestId,
-            serviced : false
-        })
+        await ledgerEntry.insertOne({
+            transactionId : req.requestId,
+            sender : req.body.from,
+            reciever : req.body.to,
+            amount : req.body.amount,
+            createdOn : `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} -- ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+            state : states[0]
+        });
     }
 
     next();
--- routes/makeTransaction.js
+++ routes/makeTransaction.js
@@ -1,74 +1,71 @@
 import { ObjectId } from 'mongodb';
-import { userCollection, ledgerEntry, date} from '../middleware/db.js';
+import { userCollection, ledgerEntry, date } from '../middleware/db.js';
+import { states } from '../Barrel.js';
 
-export const makeTransaction = async(req, res) => {
-        let req_data = await idempotency.findOne({transactionId: req.requestId, serviced : true});
-        if(req_data){
-            await idempotency.findOneAndDelete({transactionId : req.requestId, serviced : false});
-            return res.status(200).json(req_data.response);
-        }
-        let from = new ObjectId(req.body.from);
-        let to  = new ObjectId(req.body.to);
-        let amount = parseInt(req.body.amount);
-        let from_data = await userCollection.findOne({_id : from});
-        let to_data = await userCollection.findOne({_id : to});
-        if(from.toString() === to.toString()){
-            return res.status(400).json({
-                "msg" : "Bad request"
-            });
-        }
-        if((!from_data || !to_data )){
-            return res.status(404).json({
-                "msg" : "User Not Found!"
-            });
-        }
-        if(amount <= 0){
-            return res.status(400).json({
-                "msg" : "Please add valid amount"
-            });
-        }
-        if(from_data.balance != 0 && from_data.balance < amount){
-            return res.status(409).json({
-                "msg" : "Insufficient Balance"
-            });
-        }
-        let credit = parseInt(from_data.balance) - amount;
-        let debit = parseInt(to_data.balance) + amount;
-
-        await userCollection.findOneAndUpdate({_id : from},
-            {$set : {balance : credit}});
-
-        await userCollection.findOneAndUpdate({_id : to},
-        {$set : {balance : debit}});
-
-        let after_sender = await userCollection.findOne({_id : from});
-        let after_reciever = await userCollection.findOne({_id : to});
-        if((from_data.balance + amount !== after_sender.balance) && (to_data.balance + amount !== after_reciever.balance)){
-            return res.status(500).json({
-                "msg" : "Transaction Interuppted"
-            });
-        }
-        
-        await idempotency.findOneAndUpdate({transactionId : req.requestId, serviced : false}, {$set : {serviced : true, response : {
-            from : from,
-            To: to,
-            amount : amount,
-            date : `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
-        } }});
-        
-        await ledgerEntry.insertOne({
-            from : from,
-            To: to,
-            amount : amount,
-            date : `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
-        })
-        
-        res.status(200).json({
-            from : from,
-            To: to,
-            amount : amount,
-            date : `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
+export const makeTransaction = async (req, res) => {
+    let from = new ObjectId(req.body.from);
+    let to = new ObjectId(req.body.to);
+    let amount = parseInt(req.body.amount);
+    let from_data = await userCollection.findOne({ _id: from });
+    let to_data = await userCollection.findOne({ _id: to });
+    if (from.toString() === to.toString()) {
+        return res.status(400).json({
+            "msg": "Bad request"
         });
     }
+    if ((!from_data || !to_data)) {
+        return res.status(404).json({
+            "msg": "User Not Found!"
+        });
+    }
+    if (amount <= 0) {
+        return res.status(400).json({
+            "msg": "Please add valid amount"
+        });
+    }
+    if (from_data.balance != 0 && from_data.balance < amount) {
+        return res.status(409).json({
+            "msg": "Insufficient Balance"
+        });
+    }
+    let request_data = await ledgerEntry.findOne({ transactionId: req.requestId, state: 'Done' });
+    if (request_data) {
+        res.json({
+            status: states[2],
+            from: from,
+            To: to,
+            amount: amount,
+            creationTime: `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} -- ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+        })
+    }
+    let credit = parseInt(from_data.balance) - amount;
+    let debit = parseInt(to_data.balance) + amount;
+
+    await userCollection.findOneAndUpdate({ _id: from },
+        { $set: { balance: credit } });
+
+    await userCollection.findOneAndUpdate({ _id: to },
+        { $set: { balance: debit } });
+
+    let after_sender = await userCollection.findOne({ _id: from });
+    let after_reciever = await userCollection.findOne({ _id: to });
+    if ((from_data.balance + amount !== after_sender.balance) && (to_data.balance + amount !== after_reciever.balance)) {
+        await ledgerEntry.updateOne({ transactionId: req.requestId }, { $set: { state: states[1] } });
+        return res.status(500).json({
+            "status": states[1],
+            "msg": "Transaction Interuppted"
+        });
+    }
+
+    await ledgerEntry.updateOne({ transactionId: req.requestId }, { $set: { state: states[2] } });
+
+    res.status(200).json({
+        status: states[2],
+        from: from,
+        To: to,
+        amount: amount,
+        creationTime: `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} -- ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    });
+}
 
 // export default makeTransaction;
