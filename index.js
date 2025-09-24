705e382f2f041c6ae517b9ec27fe0b6fccbb56ff
Keval Tank <kevaltank1445@gmail.com>
2025-09-24 19:10:34 +0530

    add session

--- index.js
+++ index.js
@@ -3,64 +3,90 @@ import { PrismaClient } from './generated/prisma/client.js';
 import createHttpError from 'http-errors';
 import { v4 } from 'uuid'
 import jwt from 'jsonwebtoken'
+import session from 'express-session'
+import mongoose from 'mongoose'
+import MongoStore from 'connect-mongo'
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
 const PORT = process.env.PORT;
 const prisma = new PrismaClient();
+await mongoose.connect("mongodb://127.0.0.1:27017/sessions");
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+//setup session
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000
+    },
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://127.0.0.1:27017/sessions',
+        collectionName: 'session'
+    })
+}))
 
 //middleware
 const authenticator = (req, res, next) => {
-    let authHeader = req.headers['authorization']
-    const token = authHeader
-    if(token == null || !token){
-        return res.status(401).json({
-            "msg" : "Unauthorized"
+    try {
+        if (!req.session.user) {
+            throw createHttpError(401, "Unauthorized")
+        }
+        req.session.visited.push(req.originalUrl)
+        const token = req.headers['authorization'];
+        if (token == null || !token) {
+            throw createHttpError(401, "Unauthorized")
+        }
+        jwt.verify(token, process.env.SECRET_KEY, async (err, payload) => {
+            if (err) {
+                throw createHttpError(403, "Invalid or expired token")
+            }
+            let data = await prisma.users.findFirst({
+                where: {
+                    id: payload.id,
+                    password: payload.password
+                }
+            })
+            if (!data || data.password != payload.password) {
+
+                throw createHttpError(401, "Unauthorized")
+            }
+            req.payload = payload
+            next();
+        })
+    } catch (error) {
+        return res.status(error.statusCode || 500).json({
+            msg : error
         })
     }
-    jwt.verify(token, process.env.SECRET_KEY, async(err, payload) => {
-        if(err) {
-            return res.status(403).json({
-                "msg" : "Expired"
-            })
-        }
-        let data = await prisma.users.findFirst({
-            where : {
-                id : payload.id,
-                password: payload.password
-            }
-        })
-        if(!data || data.password != payload.password){
-            return res.status(401).json({
-                "msg" : "Unauthorized"
-            })
-        }
-        req.payload = payload
-        next();
-    })
 }
 
 //signup
-app.post('/signup', async(req, res) => {
+app.post('/signup', async (req, res) => {
     try {
         const name = req.body.name;
         const password = req.body.password
         let result = await prisma.users.create({
-            data: { name , password}
+            data: { name, password }
         })
         let payload = {
-            id : result.id,
+            id: result.id,
             password
         }
         let access_token = jwt.sign(payload, process.env.SECRET_KEY);
+        req.session.user = result.id;
+        req.session.visited = [req.originalUrl];
         return res.status(201).json({
-            "id" : result.id,
-            "name" : name,
-            "access-key" : access_token,
+            "id": result.id,
+            "name": name,
+            "access-token" : access_token,
             "balance": result.balance,
-            "createdOn" : result.createdOn
+            "createdOn": result.createdOn
         });
     } catch (error) {
         return res.status(500).json({
@@ -85,7 +111,7 @@ app.post('/signup', async(req, res) => {
 // })
 
 // get balance
-app.get('/balance',authenticator, async (req, res) => {
+app.get('/balance', authenticator, async (req, res) => {
     try {
         const id = parseInt(req.payload.id)
         const data = await prisma.users.findFirst({
@@ -95,8 +121,8 @@ app.get('/balance',authenticator, async (req, res) => {
             throw createHttpError(404, `User with id ${id} was not found`)
         }
         return res.status(200).json({
-            "id" : data.id,
-            "balance" : data.balance
+            "id": data.id,
+            "balance": data.balance
         });
     } catch (error) {
         return res.status(error.statusCode).json({
@@ -106,7 +132,7 @@ app.get('/balance',authenticator, async (req, res) => {
 })
 
 // fund
-app.post('/fund',authenticator, async (req, res) => {
+app.post('/fund', authenticator, async (req, res) => {
     try {
         const id = parseInt(req.payload.id)
         const amount = parseInt(req.body.amount)
@@ -125,8 +151,8 @@ app.post('/fund',authenticator, async (req, res) => {
             data: { balance: curr_balance + amount }
         })
         return res.status(200).json({
-            "id" : id,
-            "balance" : updated_data.balance
+            "id": id,
+            "balance": updated_data.balance
         })
     } catch (error) {
         return res.status(error.statusCode || 500).json({
@@ -136,43 +162,43 @@ app.post('/fund',authenticator, async (req, res) => {
 })
 
 //transfer
-app.post('/transfer',authenticator, async (req, res) => {
+app.post('/transfer', authenticator, async (req, res) => {
     try {
         let sender_id = parseInt(req.payload.id)
         let reciever_id = parseInt(req.body.reciever)
         let transaction_id = v4();
         let transfer_amount = parseInt(req.body.amount)
         let sender_details = await prisma.users.findFirst({
-            where : {id : sender_id}
+            where: { id: sender_id }
         })
         let reciever_details = await prisma.users.findFirst({
-            where : {id : reciever_id}
+            where: { id: reciever_id }
         })
-        if(sender_id === reciever_id || transfer_amount <= 0 || sender_id === NaN || reciever_id === NaN){
+        if (sender_id === reciever_id || transfer_amount <= 0 || sender_id === NaN || reciever_id === NaN) {
             throw createHttpError(400, "Bad request")
         }
-        if(!sender_details ||  !reciever_details){
+        if (!sender_details || !reciever_details) {
             throw createHttpError(404, "User was not found!")
         }
-        if(sender_details.balance < transfer_amount){
+        if (sender_details.balance < transfer_amount) {
             throw createHttpError(409, "Insufficient balance, unable to transfer")
         }
         let past_transaction = await prisma.ledger.findFirst({
-            where : {
+            where: {
                 transactionId: transaction_id,
-                status : 'Done'
+                status: 'Done'
             }
         })
-        if(past_transaction){
+        if (past_transaction) {
             return res.status(200).json(past_transaction)
         }
         // create a ledger entry
         await prisma.ledger.create({
-            data : {
-                transactionId : transaction_id,
-                sender : sender_id,
-                reciever : reciever_id,
-                amount : transfer_amount
+            data: {
+                transactionId: transaction_id,
+                sender: sender_id,
+                reciever: reciever_id,
+                amount: transfer_amount
             }
         })
         let sender_balance = sender_details.balance;
@@ -180,26 +206,26 @@ app.post('/transfer',authenticator, async (req, res) => {
         //transaction
         await prisma.$transaction([
             prisma.users.update({
-                where : {id : sender_id},
-                data : {balance : sender_balance - transfer_amount}
+                where: { id: sender_id },
+                data: { balance: sender_balance - transfer_amount }
             }),
             prisma.users.update({
-                where : {id : reciever_id},
-                data : {balance : reciever_balance + transfer_amount}
+                where: { id: reciever_id },
+                data: { balance: reciever_balance + transfer_amount }
             }),
             prisma.ledger.update({
-                where : {
-                    transactionId : transaction_id,
-                    status : 'Pending'
+                where: {
+                    transactionId: transaction_id,
+                    status: 'Pending'
                 },
-                data : {
-                    status : 'Done'
+                data: {
+                    status: 'Done'
                 }
             })
-        ])  
+        ])
         let updated_ledger_entry = await prisma.ledger.findFirst({
-            where : { transactionId : transaction_id }
-        }) 
+            where: { transactionId: transaction_id }
+        })
         return res.status(200).json(updated_ledger_entry);
     } catch (error) {
         return res.status(error.statusCode).json({
@@ -209,10 +235,10 @@ app.post('/transfer',authenticator, async (req, res) => {
 })
 
 // signout
-app.get('/signout',authenticator,(req, res) => {
-    req.headers['authorization'] = '';        
+app.get('/signout', authenticator, (req, res) => {
+    req.headers['authorization'] = '';
     res.status(200).json({
-        "msg" : "You've logged out"
+        "msg": "You've logged out"
     })
 })
 
