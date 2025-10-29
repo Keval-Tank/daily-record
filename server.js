804c218a9462eef46ef63f3c9052de2bc6d6c2c2
Innew Desktop3 <innewdesktop3@Innews-Mac-mini-2.local>
2025-10-29 18:02:23 +0530

    initail commit

---
+++ server.js
@@ -0,0 +1,214 @@
+import express, { urlencoded } from 'express'
+import supabase from './supabaseClient/supabaseClient.js'
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(urlencoded({extended : true}))
+
+let user_id;
+
+class AppError extends Error{
+    constructor(message, statusCode){
+        super(message)
+        this.statusCode = statusCode
+        this.isOperational = true
+    }
+}
+
+// register and automatic-login 
+app.post('/signup', async(req, res) => {
+    try{
+        const {email, password} = req.body
+        if(!email || !password){
+            throw new AppError('Invalid input', 400)
+        }
+        const {data, error} = await supabase.auth.signUp({
+            email,
+            password
+        })
+        if(error){
+            throw new AppError(error.message, 500)
+        }
+        if(data.user === null || data.session == null){
+            throw new AppError("Failed to register", 500)
+        }
+        if(data){
+            user_id = data.user.id
+            return res.status(201).json(data)
+        }
+    }catch(err){
+        return res.status(err.statusCode || 400).json({
+            "msg" : err.message
+        })
+    }
+})
+
+// login
+app.post('/login', async(req, res) => {
+    try{
+        const {email, password} = req.body
+        if(!email || !password){
+            throw new AppError('invalid input', 400)
+        }
+        const {data, error} = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        if(error){
+            throw new AppError(error.message, 500)
+        }
+        if(data){
+            user_id = data.user.id
+            return res.status(200).json({
+                "id" : user_id
+            })
+        }
+    }catch(err){
+        return res.status(err.statusCode || 400).json({
+            "msg" : err.message
+        })
+    }
+})
+
+// create note
+app.post('/create-note', async(req, res) => {
+    try{
+        if(!user_id){
+            throw new AppError('Unauthorized', 401)
+        }
+        const {title, content} = req.body
+        const {data, error} = await supabase.from('notes').insert([{writer : user_id,title, content}]).select()
+        if(error){
+            throw new AppError(error.message, 500)
+        }
+        return res.status(201).json(data)  
+    }catch(err){
+        return res.status(err.statusCode || 500).json({
+            "msg" : err.message
+        })
+    }
+})
+
+// get data 
+app.get('/get-notes', async(req, res) => {
+   try{
+        if(!user_id){
+            throw new AppError('Unauthorized', 401)
+        }
+        const {data, error} = await supabase.from('notes').select().eq('writer',user_id)
+        if(error){
+            throw new AppError('Data nou found!', 404)
+        }
+        if(data.length === 0){
+            return res.status(200).json("You've not created any notes yet!")
+        }
+        if(data){
+        return res.status(200).json(data)   
+        }
+   }catch(err){
+        return res.status(err.statusCode || 400).json({
+            "msg" : err.message
+        }) 
+   }
+})
+
+// update note
+app.put('/update-note/:id', async(req, res) => {
+    try{
+        let writer_id;
+        if(!user_id){
+            throw new AppError('Unauthorized', 401)
+        }
+        const id = req.params.id
+        const {nt, new_content} = req.body
+        const {data} = await supabase.from('notes').select().eq('note_id', id)
+        if(data.length === 0){
+            throw new AppError('Data not found!',404)
+        }
+        if(data){
+           writer_id = data[0].writer;
+        }
+        if(writer_id !== user_id){
+            throw new AppError('Unauthorized to update note',403)
+        }
+        const {error} = await supabase.from('notes').update({title : nt, content : new_content}).eq('note_id', id)
+        if(error){
+            throw new AppError('Internal server error', 500)
+        }
+        return res.status(200).json({
+            "note_id" : data[0].note_id,
+            "writer" : data[0].writer,
+            "title" : nt,
+            "content" : new_content,
+            "created_at" : data[0].created_at
+        })
+    }catch(err){
+        return res.status(err.statusCode || 500).json({
+            "msg" : err.message
+        })
+    }
+})
+
+// delete note
+app.delete('/delete/:id', async(req, res) => {
+    try{
+        if(!user_id){
+            throw new AppError('Unauthorized', 401)
+        }
+        const note_id = req.params.id
+        const {data} = await supabase.from('notes').select().eq('note_id', note_id)
+        if(data.length === 0){
+            throw new AppError('Data not found', 404)
+        }
+        if(data[0].writer !== user_id){
+            throw new AppError('Unauthorized to delete', 403)
+        }
+        const {error} = await supabase.from('notes').delete().eq('note_id', note_id)
+        if(error){
+            throw new AppError('Server error', 500)
+        }
+        return res.status(200).json({
+            "note_id" : data[0].note_id,
+            "writer" : data[0].writer,
+            "title" : data[0].title,
+            "content" : data[0].content,
+            "created_at" : data[0].created_at
+        })
+    }catch(err){
+        return res.status(err.statusCode || 500).json({
+            "msg" : err.message
+        })
+    }
+})
+
+//check sessions
+app.get('/sessions', async(req, res) => {
+    const {data : {session},} = await supabase.auth.getSession();
+    return res.json({
+        "session" : session
+    })
+})
+
+// signout
+app.get('/signout', async(req, res) => {
+    try{
+        if(!user_id){
+            throw new AppError('Unauthorized', 401)
+        }
+        const {error} = await supabase.auth.signOut();
+        if(error === null){
+            user_id = ''
+            return res.status(204)
+        }else{
+            throw new AppError('Internal Server Error', 500)
+        }
+    }catch(err){
+        return res.status(err.statusCode || 500).json({
+            "msg" : err.message
+        })
+    }
+})
+
+app.listen(PORT, () => {console.log(`Server running on ${PORT}`)})
